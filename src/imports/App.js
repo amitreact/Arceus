6b06e7b2ac1aa2f85234d6e6edcf274abe3b49ba
amitreact <40492379+amitreact@users.noreply.github.com>
2018-06-22 18:02:11 +0530

    Login Page

--- src/imports/App.js
+++ src/imports/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import Login from "./pages/Login";
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <button />
+        <MuiThemeProvider>
+          <Login />
+        </MuiThemeProvider>
       </div>
     );
   }
